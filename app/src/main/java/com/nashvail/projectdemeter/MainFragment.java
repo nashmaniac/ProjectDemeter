261896546edd9d5a87e05f099fee779c295a96c0
nashmaniac <nishantve1@gmail.com>
2015-05-17 16:21:36 +0530

    Added a custom class to detect swipes on views

--- app/src/main/java/com/nashvail/projectdemeter/MainFragment.java
+++ app/src/main/java/com/nashvail/projectdemeter/MainFragment.java
@@ -4,9 +4,11 @@ package com.nashvail.projectdemeter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*
 * Class : MainFragment
@@ -16,7 +18,9 @@ import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 
+    private static final String TAG = MainFragment.class.getSimpleName();
     private TextView mAmountView;
+
 
     public MainFragment() {
         // Required empty public constructor
@@ -31,6 +35,13 @@ public class MainFragment extends Fragment {
         mAmountView = (TextView) v.findViewById(R.id.text_view_amount);
 
         // we will have to detect the swipes and taps on this view right here
+        mAmountView.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Toast.makeText(getActivity(), "The view was touched", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
         return v;
     }
