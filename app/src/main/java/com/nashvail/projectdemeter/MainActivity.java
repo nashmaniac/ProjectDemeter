17e96ccd43fc70ed9e701e02de08273a29b07622
nashmaniac <nishantve1@gmail.com>
2015-05-17 09:10:56 +0530

    Initial layout taking shape

--- app/src/main/java/com/nashvail/projectdemeter/MainActivity.java
+++ app/src/main/java/com/nashvail/projectdemeter/MainActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
